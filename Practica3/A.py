c23acb876c4f34d40e331b52813e97c3806cb3a1
Alan Achaval <achaval.alan.n@gmail.com>
2019-06-15 00:53:36 -0300

    Fix A

--- Practica3/A.py
+++ Practica3/A.py
@@ -1,11 +1,14 @@
+import math
+
+
 class A:
 
     def __init__(self, eta, b, psi, c):
         self.dos = (1 + psi.dos * eta.n) / (psi.tres - psi.dos) * (
-                ((psi.cero - psi.tres) * (c.cero ** ((psi.cero - psi.dos) ** b))) / (psi.cero * eta.n + 1) +
-                ((psi.uno - psi.tres) * (c.uno ** ((psi.uno - psi.dos) ** b))) / (psi.uno * eta.n + 1)
+                ((psi.cero - psi.tres) * c.cero * (math.e ** ((psi.cero - psi.dos) * b))) / (psi.cero * eta.n + 1) +
+                ((psi.uno - psi.tres) * c.uno * (math.e ** ((psi.uno - psi.dos) * b))) / (psi.uno * eta.n + 1)
         )
         self.tres = -(1 + psi.tres * eta.n) / (psi.tres - psi.dos) * (
-                ((psi.cero - psi.dos) * (c.cero ** ((psi.cero - psi.tres) ** b))) / (psi.cero * eta.n + 1) +
-                ((psi.uno - psi.dos) * (c.uno ** ((psi.uno - psi.tres) ** b))) / (psi.uno * eta.n + 1)
+                ((psi.cero - psi.dos) * c.cero * (math.e ** ((psi.cero - psi.tres) * b))) / (psi.cero * eta.n + 1) +
+                ((psi.uno - psi.dos) * c.uno * (math.e ** ((psi.uno - psi.tres) * b))) / (psi.uno * eta.n + 1)
         )
